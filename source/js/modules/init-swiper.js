b53aa78d9093ab826465fcebe2e31402d0cbfe21
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2023-05-21 21:23:51 +0300

    fix bugs

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -43,6 +43,7 @@ const initReviewsSwiper = () => {
       new Swiper('.reviews__swiper', {
         direction: 'horizontal',
         loop: false,
+        autoHeight: true,
         slidesPerView: 1,
         initialSlide: 0,
         spaceBetween: 33,
